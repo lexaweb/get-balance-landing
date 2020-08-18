067125a22f1e84573b41ab707d6471ba0f37a354
lexaweb <59533947+lexaweb@users.noreply.github.com>
2020-08-18 14:57:46 +0300

    правка хедера

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import $ from "jquery";
+import $ from 'jquery';
 import '@fancyapps/fancybox';
 import Inputmask from 'inputmask';
 import 'nouislider';
@@ -6,22 +6,51 @@ import 'select2';
 import Swiper from 'swiper/bundle';
 import 'tooltipster';
 import './_backend';
+// import './ScrollMagic';
 
 $(document).ready(() => {
-  $('select').select2({
-    minimumResultsForSearch: -1
-  });
+  // $('select').select2({
+  //   minimumResultsForSearch: -1
+  // });
 
-  var mySwiper = new Swiper('.swiper-container', {
-    speed: 400,
-    spaceBetween: 100,
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  // var mySwiper = new Swiper('.swiper-container', {
+  //   speed: 400,
+  //   spaceBetween: 100,
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     type: 'bullets',
+  //   },
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   },
+  // });
+
+  // var scrolled = $(this).scrollTop();
+  //   if (scrolled = 0) {
+  //     alert("вверх")
+  //   } else {
+  //     alert("вниз")
+  //   }
+  //   $( '.funtimes' ).animate({
+  //     left: '+=50', // увеличить на 50
+  //     opacity: 0.25
+  //   },
+  //   300,
+  // );
+
+  // $(".funtimes").scroll(function() {
+  //   alert("вниз")
+  // });
+
+  // init controller
+// var controller = new ScrollMagic.Controller();
+
+// create a scene
+// new ScrollMagic.Scene({
+//     duration: 100, // the scene should last for a scroll distance of 100px
+//     offset: 50 // start this scene after scrolling for 50px
+// })
+//     .setPin('#my-sticky-element') // pins the element for the the scene's duration
+//     .addTo(controller); // assign the scene to the controller
 });
