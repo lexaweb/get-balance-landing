ec722406dc2430bfbe9a5b1750a0375a612fc43c
lexaweb <59533947+lexaweb@users.noreply.github.com>
2020-08-19 15:39:45 +0300

    создание анимации первой секции

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,8 @@ import 'select2';
 import Swiper from 'swiper/bundle';
 import 'tooltipster';
 import './_backend';
-// import './ScrollMagic';
+// import 'skrollr';
+// import '../js/skrollr'
 
 $(document).ready(() => {
   // $('select').select2({
@@ -26,31 +27,6 @@ $(document).ready(() => {
   //   },
   // });
 
-  // var scrolled = $(this).scrollTop();
-  //   if (scrolled = 0) {
-  //     alert("вверх")
-  //   } else {
-  //     alert("вниз")
-  //   }
-  //   $( '.funtimes' ).animate({
-  //     left: '+=50', // увеличить на 50
-  //     opacity: 0.25
-  //   },
-  //   300,
-  // );
+  skrollr.init();
 
-  // $(".funtimes").scroll(function() {
-  //   alert("вниз")
-  // });
-
-  // init controller
-// var controller = new ScrollMagic.Controller();
-
-// create a scene
-// new ScrollMagic.Scene({
-//     duration: 100, // the scene should last for a scroll distance of 100px
-//     offset: 50 // start this scene after scrolling for 50px
-// })
-//     .setPin('#my-sticky-element') // pins the element for the the scene's duration
-//     .addTo(controller); // assign the scene to the controller
 });
