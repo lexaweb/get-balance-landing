abe3be849ec905dc76f761dc06a71f734815ee77
lexaweb <59533947+lexaweb@users.noreply.github.com>
2020-08-21 15:19:35 +0300

    создание анимации 2 блока, завершил анимацию первого

--- gulp-tasks/pug2html.js
+++ gulp-tasks/pug2html.js
@@ -11,22 +11,22 @@ const gulp = require('gulp'),
 module.exports = function pug2html() {
   return gulp.src('src/pages/**/*.pug')
     .pipe(plumber())
-    .pipe(cached('pugCache'))
-    .pipe(dependents({
-      ".pug": {
-        parserSteps: [
-          /^\s*(?:extends|include)\s+(.+?)\s*$/gm,
-          function (str) {
-            var absolute = str.match(/^[\\/]+(.+)/);
-            if (absolute) {
-              str = path.resolve('src/pages', absolute[1]);
-            }
-            return [str];
-          }
-        ],
-        postfixes: ['.pug', '.jade']
-      }
-    }))
+    // .pipe(cached('pugCache'))
+    // .pipe(dependents({
+    //   ".pug": {
+    //     parserSteps: [
+    //       /^\s*(?:extends|include)\s+(.+?)\s*$/gm,
+    //       function (str) {
+    //         var absolute = str.match(/^[\\/]+(.+)/);
+    //         if (absolute) {
+    //           str = path.resolve('src/pages', absolute[1]);
+    //         }
+    //         return [str];
+    //       }
+    //     ],
+    //     postfixes: ['.pug', '.jade']
+    //   }
+    // }))
     .pipe(pugLinter({ reporter: 'default' }))
     .pipe(filter('src/pages/*.pug'))
     .pipe(pug({ pretty: true }))
